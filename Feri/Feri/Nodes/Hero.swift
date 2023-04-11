28b9ef13d79f6587c2a1b11050e72e643022f8a6
otavioalbucosta <otavio-albucosta@hotmail.com>
2023-04-11 11:52:07 -0300

    button with ambient interaction added :)

--- Feri/Feri/Nodes/Hero.swift
+++ Feri/Feri/Nodes/Hero.swift
@@ -15,9 +15,12 @@ class Hero: SKSpriteNode {
     var atlasFront = SKTextureAtlas(named: "HeroFrontTest")
     var atlasBack = SKTextureAtlas(named: "HeroBackTest")
     private(set) var currentPosition: Point
+    private(set) var facingPosition: Point
     var hitbox: CGSize
     init(currentPosition: Point) {
         self.currentPosition = currentPosition
+        self.facingPosition = currentPosition
+        facingPosition.x -= 1
         self.hitbox = CGSize(width: atlasFront.textureNamed(atlasFront.textureNames.first!).size().width, height: atlasFront.textureNamed(atlasFront.textureNames.first!).size().height/4)
         super.init(texture: atlasFront.textureNamed(atlasFront.textureNames[0]), color: .clear, size: atlasFront.textureNamed(atlasFront.textureNames[0]).size())
         
@@ -32,12 +35,12 @@ class Hero: SKSpriteNode {
         let children = map.floor.children.map { child in
             child as! InteractibleItem
         }
-        print(children.first!.tileMapPosition)
-        if children.first(where: {$0.tileMapPosition == position}) == nil {
+        
+        
+        if children.first(where: {$0.tileMapPosition == position}) == nil && position != currentPosition{
             var gridPosition = map.floor.centerOfTile(atColumn: position.y, row: position.x)
             gridPosition.y += self.size.height/3
             let moveAction =  SKAction.move(to: gridPosition, duration: 0.5)
-            print(position)
             self.run(moveAction)
             var animateAction: SKAction
             switch direction {
@@ -45,31 +48,67 @@ class Hero: SKSpriteNode {
                 animateAction = SKAction.animate(with: atlasBack.textureNames.map(SKTexture.init(imageNamed:)), timePerFrame: 0.1)
                 self.xScale = 0.3
                 self.run(animateAction)
+                facingPosition = position
+                facingPosition.y -= 1
             case .TopRight:
                 animateAction = SKAction.animate(with: atlasBack.textureNames.map(SKTexture.init(imageNamed:)), timePerFrame: 0.1)
                 self.xScale = -0.3
                 self.run(animateAction)
+                facingPosition = position
+                facingPosition.x += 1
             case .DownLeft:
                 animateAction = SKAction.animate(with: atlasFront.textureNames.map(SKTexture.init(imageNamed:)), timePerFrame: 0.1)
                 self.xScale = 0.3
                 self.run(animateAction)
+                facingPosition = position
+                facingPosition.x -= 1
             case .DownRight:
                 animateAction = SKAction.animate(with: atlasFront.textureNames.map(SKTexture.init(imageNamed:)), timePerFrame: 0.1)
                 self.xScale = -0.3
                 self.run(animateAction)
+                facingPosition = position
+                facingPosition.y += 1
             case .None:
                 break
             
             }
             self.currentPosition = position
+        }else {
+            switch direction {
+            case .TopLeft:
+                self.texture = atlasBack.textureNamed(atlasBack.textureNames.first!)
+                self.xScale = 0.3
+                facingPosition = currentPosition
+                facingPosition.y -= 1
+            case .TopRight:
+                self.texture = atlasBack.textureNamed(atlasBack.textureNames.first!)
+                self.xScale = -0.3
+                facingPosition = currentPosition
+                facingPosition.x += 1
+            case .DownLeft:
+                self.texture = atlasFront.textureNamed(atlasFront.textureNames.first!)
+                self.xScale = 0.3
+                facingPosition = currentPosition
+                facingPosition.x -= 1
+            case .DownRight:
+                self.texture = atlasFront.textureNamed(atlasFront.textureNames.first!)
+                self.xScale = -0.3
+                facingPosition = currentPosition
+                facingPosition.y += 1
+            case .None:
+                break
+            }
         }
-        
 
     }
     
-    func interactWith(object: InteractibleItem) {
-        if object.compatiblePickableItems != nil {
-            
+    func interactWithObject(on map: levelMapProtocol) {
+        print("oia")
+        let objects = map.floor.children.map { obj in
+            obj as! InteractibleItem
+        }
+        if let obj = objects.first(where: {$0.tileMapPosition == facingPosition}) {
+            obj.interact()
         }
             
     }
