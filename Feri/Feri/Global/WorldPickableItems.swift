46e25a5c10dcb1b3da20f42f27edac56e6bc8e4d
otavioalbucosta <otavio-albucosta@hotmail.com>
2023-04-26 18:09:03 -0300

    merged dev with feature/inventory-system

--- Feri/Feri/Global/WorldPickableItems.swift
+++ Feri/Feri/Global/WorldPickableItems.swift
@@ -34,21 +34,21 @@ class WorldPickableItems {
     }
 
     func generatePickableItems() -> [PickableItem] {
-        let balconyKey = PickableItem(name: "balconyKey", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-chave"))
+        let balconyKey = PickableItem(name: "balconyKey", remainingUses: 1, textureName: "TileSet-chave")
 
-        let bathroomKey = PickableItem(name: "bathroomKey", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-chaveBanheiro"))
+        let bathroomKey = PickableItem(name: "bathroomKey", remainingUses: 1, textureName: "TileSet-chaveBanheiro")
 
-        let crowbar = PickableItem(name: "crowbar", remainingUses: 2, texture: SKTexture(imageNamed: "crowbar"))
+        let crowbar = PickableItem(name: "crowbar", remainingUses: 2, textureName: "crowbar")
 
-        let ropes = PickableItem(name: "ropes", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-corda"))
+        let ropes = PickableItem(name: "ropes", remainingUses: 1, textureName: "TileSet-corda")
 
-        let lantern = PickableItem(name: "lantern", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-lanterna"))
+        let lantern = PickableItem(name: "lantern", remainingUses: 1, textureName: "TileSet-lanterna")
 
-        let plunger = PickableItem(name: "plunger", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-chaveBanheiro"))
+        let plunger = PickableItem(name: "plunger", remainingUses: 1, textureName: "TileSet-chaveBanheiro")
 
-        let storageKey = PickableItem(name: "storageKey", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-chave"))
+        let storageKey = PickableItem(name: "storageKey", remainingUses: 1, textureName: "TileSet-chave")
 
-        let stool = PickableItem(name: "stool", remainingUses: 1, texture: SKTexture(imageNamed: "TileSet-banquinho"))
+        let stool = PickableItem(name: "stool", remainingUses: 1, textureName: "TileSet-banquinho")
 
         return [
             balconyKey,
